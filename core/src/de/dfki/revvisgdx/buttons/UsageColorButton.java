c83155efa7804850997da647ac9175006d409ccc
Jannis Stoppe <jstoppe@informatik.uni-bremen.de>
2016-12-20 16:41:58 +0100

    UsageColorButton is now down to 0 checkstyle errors

--- core/src/de/dfki/revvisgdx/buttons/UsageColorButton.java
+++ core/src/de/dfki/revvisgdx/buttons/UsageColorButton.java
@@ -3,18 +3,32 @@ package de.dfki.revvisgdx.buttons;
 import de.dfki.revvisgdx.Button;
 import de.dfki.revvisgdx.RevVisGDX;
 
+/**
+ * A button class that takes the original functionality from the
+ * de.dfki.revvisgdx.buttons.Button class and turns it into a three-way toggle
+ * button to cycle through the available coloring methods.
+ */
 public class UsageColorButton extends Button {
+
+	/**
+	 * Creates a new UsageColorButton.
+	 * There shouldn't really be more than one.
+	 */
 	public UsageColorButton() {
 		super("data/VariableButtonUsageNone.png");
-		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Cycles the RevVisGDX.singleton's line usage coloring value and
+	 * replaces this button's texture accordingly.
+	 */
 	@Override
 	public void Clicked() {
 		super.Clicked();
-		
+
 		RevVisGDX.singleton.currentCircuit.toggleLineUsageColouring();
-		switch(RevVisGDX.singleton.currentCircuit.drawLinesColourizedWhenUsed) {
+		switch
+			(RevVisGDX.singleton.currentCircuit.drawLinesColourizedWhenUsed) {
 		case absolute:
 			this.setTexture("data/VariableButtonUsageAbsolute.png");
 			break;
